e5d7f55eb3aab5166d404fdccbc67c560076dd16
keatlaretseM <116970363+keatlaretseM@users.noreply.github.com>
2022-10-29 21:30:38 +0200

    Add files via upload

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://mediabug.africa/</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://mediabug.africa/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://mediabug.africa/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://mediabug.africa/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://mediabug.africa/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://mediabug.africa/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
